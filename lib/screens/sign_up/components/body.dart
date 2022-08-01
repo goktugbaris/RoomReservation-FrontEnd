237ccc6d0cb45143b988f65769b5d25dea1e704f
unknown <goktugbaris02@gmail.com>
2022-08-01 10:28:05 +0300

    Comp. Sign-Up Page

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,50 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:room_reservation/constants.dart';
-import 'package:room_reservation/size_config.dart';
+import '../../../constants.dart';
+import '../../../size_config.dart';
+import 'sign_up_form.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            "Create Account",
-            style: headingStyle,
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04), // 4%
+                Text("Create Account", style: headingStyle),
+                Text(
+                  "Create an account to get all features",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignUpForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+              ],
+            ),
           ),
-          Text(
-            "Create an account to get all features",
-            textAlign: TextAlign.center,
-          ),
-          SignUpForm(),
-        ],
+        ),
       ),
-    );
-  }
-}
-
-class SignUpForm extends StatefulWidget {
-  const SignUpForm({Key? key}) : super(key: key);
-
-  @override
-  State<SignUpForm> createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  final _formKey = GlobalKey<FormState>();
-  late String email;
-  late String password;
-  late String conform_password;
-  final List<String> errors = [];
-  
-  @override
-  Widget build(BuildContext context) {
-    return Form(child: Column(
-      children: [],
-    ),
     );
   }
 }
